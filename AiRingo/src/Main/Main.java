2ec309b2610b1f9c768e973e3e5a3dec4c540640
Bowdown4488 <paolo_ato@dlsu.edu.ph>
2019-12-08 23:30:26 +0800

    keywords

--- AiRingo/src/Main/Main.java
+++ AiRingo/src/Main/Main.java
@@ -70,15 +70,102 @@ public class Main
                 String tokenText = token.getText();
                 if(tokenText.equals("printo")){
                     tokenText = "print";
-                }else if(tokenText.equals("pubriko")){
-                    tokenText = "public";
-                }else if(tokenText.equals("boido")){
-                    tokenText = "void";
+                }else if(tokenText.equals("abstrakto")){
+                    tokenText = "abstract";
+                }else if(tokenText.equals("boorean")){
+                    tokenText = "boolean";
+                }else if(tokenText.equals("bureko")){
+                    tokenText = "break";
+                }else if(tokenText.equals("byto")){
+                    tokenText = "byte";
+                }else if(tokenText.equals("kesu")){
+                    tokenText = "case";
+                }else if(tokenText.equals("katchu")){
+                    tokenText = "catch";
+                }else if(tokenText.equals("karu")){
+                    tokenText = "char";
                 }else if(tokenText.equals("kurasu")){
                     tokenText = "class";
+                }else if(tokenText.equals("kontinu")){
+                    tokenText = "continue";
+                }else if(tokenText.equals("deporuto")){
+                    tokenText = "default";
+                }else if(tokenText.equals("do")){
+                    tokenText = "do";
+                }else if(tokenText.equals("doburu")){
+                    tokenText = "double";
+                }else if(tokenText.equals("erusu")){
+                    tokenText = "else";
+                }else if(tokenText.equals("enumu")){
+                    tokenText = "enum";
+                }else if(tokenText.equals("extendo")){
+                    tokenText = "extends";
+                }else if(tokenText.equals("finar")){
+                    tokenText = "final";
+                }else if(tokenText.equals("finarri")){
+                    tokenText = "finally";
+                }else if(tokenText.equals("furoto")){
+                    tokenText = "float";
+                }else if(tokenText.equals("foru")){
+                    tokenText = "for";
+                }else if(tokenText.equals("ifu")){
+                    tokenText = "if";
+                }else if(tokenText.equals("impremento")){
+                    tokenText = "implements";
+                }else if(tokenText.equals("importo")){
+                    tokenText = "import";
+                }else if(tokenText.equals("insutaof")){
+                    tokenText = "instanceof";
                 }else if(tokenText.equals("into")){
                     tokenText = "int";
+                }else if(tokenText.equals("intefeseru")){
+                    tokenText = "interface";
+                }else if(tokenText.equals("rongu")){
+                    tokenText = "long";
+                }else if(tokenText.equals("netivu")){
+                    tokenText = "native";
+                }else if(tokenText.equals("newu")){
+                    tokenText = "new";
+                }else if(tokenText.equals("pakeju")){
+                    tokenText = "package";
+                }else if(tokenText.equals("privatto")){
+                    tokenText = "private";
+                }else if(tokenText.equals("protekto")){
+                    tokenText = "protected";
+                }else if(tokenText.equals("pubriko")){
+                    tokenText = "public";
+                }else if(tokenText.equals("returno")){
+                    tokenText = "return";
+                }else if(tokenText.equals("shorto")){
+                    tokenText = "short";
+                }else if(tokenText.equals("statiku")){
+                    tokenText = "static";
+                }else if(tokenText.equals("strictofp")){
+                    tokenText = "strictfp";
+                }else if(tokenText.equals("supa")){
+                    tokenText = "super";
+                }else if(tokenText.equals("switchu")){
+                    tokenText = "switch";
+                }else if(tokenText.equals("sinkuronaizu")){
+                    tokenText = "synchronized";
+                }else if(tokenText.equals("disu")){
+                    tokenText = "this";
+                }else if(tokenText.equals("throwu")){
+                    tokenText = "throw";
+                }else if(tokenText.equals("throwsu")){
+                    tokenText = "throws";
+                }else if(tokenText.equals("transento")){
+                    tokenText = "transient";
+                }else if(tokenText.equals("tryu")){
+                    tokenText = "try";
+                }else if(tokenText.equals("boido")){
+                    tokenText = "void";
+                }else if(tokenText.equals("voratiru")){
+                    tokenText = "volatile";
+                }else if(tokenText.equals("wairu")){
+                    tokenText = "while";
                 }
+
             newCode += tokenText + " ";
             }
         }
